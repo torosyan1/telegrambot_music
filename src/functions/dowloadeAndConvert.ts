c23fd276f37deb8ddff81b4d3d52d445ee4f3817
vahag_t <vahag@steadfast.tech>
2021-03-01 17:34:41 +0400

    asyc fix

--- src/functions/dowloadeAndConvert.ts
+++ src/functions/dowloadeAndConvert.ts
@@ -9,6 +9,7 @@ import { app } from "../firebase/index";
 export const downloadAndConvert = async (query) => {
   try {
     let file;
+    let name;
     let newName;
 
     const video = await youtubedl(
@@ -20,22 +21,22 @@ export const downloadAndConvert = async (query) => {
     );
 
 
-    await video.on("info", (info) => {
+    await video.on("info",async (info) => {
       console.log("Download started");
       console.log("filename: " + info._filename);
       console.log("size: " + info.size);
-      const name = info._filename;
+      name = info._filename;
       name.replace(".mp4", ".mp3");
       const arr = name.split(".");
       arr[arr.length - 1] = "mp3";
       newName = arr.join(".");
+      await video.pipe(fs.createWriteStream(info._filename));
     });
-    await video.pipe(fs.createWriteStream("myvideo.mp4"));
     
     video.on("end", async () => {
       console.log("finished downloading!");
 
-      await ffmpeg("/app/myvideo.mp4")
+      await ffmpeg(`/app/$name}.mp4`)
         .withAudioCodec("libmp3lame")
         .toFormat("mp3")
         .saveToFile(`/app/${newName}`)
